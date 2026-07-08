ee69fb1f218d36c35b7f3f5b814df190269d9bcd
jdsciencelux <jd@sciencelux.org>
2026-07-07 21:10:20 -0500

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
-
+console.log(process.env.RESEND_API_KEY ? "API Key Loaded":"API Key Missing");
   if (req.method !== "POST") {
     return res.status(405).json({
       message: "Method Not Allowed",
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
   try {
 
-    await resend.emails.send({
+    const{data, error}=await resend.emails.send({
       from: "Latex Booking <onboarding@resend.dev>",
       to: "djspitroast@gmail.com",
       subject: "New Latex Repair Booking",
@@ -27,12 +27,24 @@ Phone: ${phone}
 
 Description:
 ${description}
-      `,
-    });
+`,
+});
 
-    return res.status(200).json({
-      success: true,
-    });
+console.log("Resend data:", data);
+console.log("Resend error:", error);
+
+if(error){
+return res.status(500).json({
+
+  success: false,
+  error,
+});
+
+}
+
+return res.status(200).json({
+  success: true,
+ });
 
   } catch (error) {
 
